98f13473e908d23f62d5322bf901833c65884ef9
Justin <lunstiaRBX@gmail.com>
2022-10-08 00:33:55 -0400

    Add HTML, CSS, and JS w skeleton

---
+++ app.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+function Book(title, author, description, image, hasRead) {
+  this.title = title;
+  this.author = author;
+  this.description = description;
+  this.image = image;
+  this.hasRead = hasRead;
+}
+
+Book.prototype.setRead = function(hasRead) {
+    self.hasRead = hasRead;
+}
+
+Book.prototype.Get
+
+function addBookToLibrary() {
+  // do stuff here
+}
